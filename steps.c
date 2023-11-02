3ee456f400a1a7940a92e4ebf81f861fe7c76458
KitoMurage <146374630+KitoMurage@users.noreply.github.com>
2023-11-02 12:04:27 +0000

    steps

--- steps.c
+++ steps.c
@@ -49,24 +49,38 @@ int main() {
         printf("Error opening file\n");
         return 1;
     }
+    //defining variables
     FITNESS_DATA records[59];
     int counter=0;
     int recordCount = 0;
     char line[200];
-    while (fgets(line, sizeof(line), file)) {
+    int line_count = 0;
+
+    //geting the number of records
+    while (fgets(line, sizeof(line), file)) 
         recordCount++;
-    }
-    printf("Number of records in file: %d\n", recordCount);
     
-    //for (int i = 0; i < 3; i++) {
-        //(fgets(line, sizeof(line), file) != NULL);
-        //if (strcpy(records[counter].time, "07:30"));
-             //printf("%s/%s/%d\n", records[counter].date, records[counter].time, records[counter].steps);
-            
-    //}
+    
+    printf("Number of records in file: %d\n", recordCount);
+
+    rewind(file);
+
+    //printing out the first 3 lines
+    while (line_count < 3 && fgets(line, sizeof(line), file) != NULL) {
+        //replacing , for /
+         for (int i = 0; line[i]; i++) {
+            if (line[i] == ',') {
+                line[i] = '/';
+            }
+        }
+        printf("%s", line);
+        line_count++;
+    }
+
+
+  //closing file
     fclose(file);
     return 0;
 
 }
-
     
